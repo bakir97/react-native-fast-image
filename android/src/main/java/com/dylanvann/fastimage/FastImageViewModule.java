e4f666db97f13558e81647c2c39b9ae0b671c736
bakir97 <38113801+bakir97@users.noreply.github.com>
2023-09-09 23:52:38 +0200

    Update FastImageViewModule.java

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
@@ -2,7 +2,7 @@ package com.dylanvann.fastimage;
 
 import android.app.Activity;
 
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
